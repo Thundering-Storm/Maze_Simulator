18dcb6479c198355b562a0abc9cd68a87b8816e5
Thundering Storm <thuderingatthestorm@gmail.com>
2025-12-18 15:41:56 +0100

    starter file

---
+++ Maze Game.py
@@ -0,0 +1,39 @@
+# Maze Game
+import pygame
+from rich import print
+from sys import exit as quit
+
+SIZE = 25
+
+class player():
+    def __init__(self, x: int, y: int) -> None:
+        self.pos = [x, y]
+        self.moveDistance
+
+    def movement(self) -> None:
+        pass
+
+class platform():
+    def __init__(self, xMul: int, yMul: int) -> None:
+        self.size: int = SIZE
+        self.pos = [xMul * self.size, yMul * self.size]
+
+def Draw():
+    pass
+
+pygame.init()
+
+windowSize: tuple[int, int] = (800, 600)
+
+
+Window = pygame.display.set_mode(windowSize)
+pygame.display.set_caption("Maze game")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit('Quit the game!')
+            
+    Window.fill((45, 12, 43))
+
+    pygame.display.update()
