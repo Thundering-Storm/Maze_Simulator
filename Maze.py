e52032f140e85468a178f5a14ff15ce9fcf604e5
Thundering Storm <thuderingatthestorm@gmail.com>
2025-12-19 19:01:08 +0100

    random maze every time

--- Maze.py
+++ Maze.py
@@ -1,25 +1,21 @@
 import random
 
-def generate_maze(width, height):
-    maze = [[0 for _ in range(width)] for _ in range(height)]
+def generate_maze(height, width):
+    maze = [[0 for _ in range(height)] for _ in range(width)]
 
-    for y in range(height):
-        for x in range(width):
+    for y in range(width):
+        for x in range(height):
             if random.random() < 0.3:
                 maze[y][x] = 1
 
-    spawn_x, spawn_y = random.randint(1, width-2), random.randint(1, height-2)
-    win_x, win_y = random.randint(1, width-2), random.randint(1, height-2)
+    spawn_x, spawn_y = random.randint(1, height-2), random.randint(1, width-2)
+    win_x, win_y = random.randint(1, height-2), random.randint(1, width-2)
 
     while maze[spawn_y][spawn_x] == 1 or maze[win_y][win_x] == 1:
-        spawn_x, spawn_y = random.randint(1, width-2), random.randint(1, height-2)
-        win_x, win_y = random.randint(1, width-2), random.randint(1, height-2)
+        spawn_x, spawn_y = random.randint(1, height-2), random.randint(1, width-2)
+        win_x, win_y = random.randint(1, height-2), random.randint(1, width-2)
 
     maze[spawn_y][spawn_x] = 3
     maze[win_y][win_x] = 2
 
-    maze_str = ""
-    for row in maze:
-        maze_str += ", ".join(map(str, row)) + ", "
-
-    return maze_str.strip()
+    return maze
